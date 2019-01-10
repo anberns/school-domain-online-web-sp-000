c7dc879634cf407021a0c5599dabe4c861199278
Aaron Berns <anberns@gmail.com>
2019-01-10 14:20:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,6 +7,24 @@ class School
     @roster = {}
   end
   
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
   
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort 
+    @roster.each do |grade, names|
+      names.sort
+    end
+    @roster
+  end
   
 end
